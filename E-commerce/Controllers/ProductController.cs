28ae89122419b6c289d7f5b03d0a7d40906b41e7
Tahiri Walid <walidt579@gmail.com>
2021-01-16 22:57:31 +0100

    Ajoutez des fichiers projet.

---
+++ E-commerce/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using E_commerce.Data;
+using E_commerce.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_commerce.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly StoreContext _context;
+
+        public ProductController(StoreContext db)
+        {
+            _context = db;
+        }
+
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            var products = _context.Products.ToList();
+            return View(products);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ E-commerce/Data/StoreContext.cs
@@ -0,0 +1,19 @@
+﻿using E_commerce.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_commerce.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
